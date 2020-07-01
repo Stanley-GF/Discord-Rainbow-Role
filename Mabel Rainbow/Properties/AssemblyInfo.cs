c2630601096820b2db4156c9b98fc31f38c72f6c
Stanley <66573599+Stanley-GF@users.noreply.github.com>
2020-07-01 22:43:29 +0200

    Add files via upload

---
+++ Mabel Rainbow/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Mabel Rainbow Role")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mabel Rainbow Role")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("73fe79f9-1e67-4d1f-b484-6c1a3f3427f5")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
